43003a50d161a372a00bdc5c30278dd84c67f4e7
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-11 11:35:13 -0500

    commit tree 134fb1c4522a17fdfef1a0200c3429bd539695f8

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    
+    
+    return Math.abs(distance-42)}
+
+    function distanceFromHqInFeet(distance) {
+        return [Math.abs(distance-42)] * 264}
+
+        function distanceTravelledInFeet(num1, num2) {
+            return [Math.abs(num1 - num2)] * 264}
+
+
+
+            function calculatesFarePrice (start,destination) {
+           let travelled;
+           if(start > destination){
+               travelled = (start - destination) * 264;}
+            else{
+                travelled = (destination - start) * 264;
+            }
+
+            if(travelled < 400) {return 0;}
+
+            if(travelled >= 400 && travelled <= 2000){
+             return (travelled - 400) * .02;
+
+            }
+
+            if(travelled > 2000 && travelled <= 2500) {
+                return 25;
+            }
+           else
+           {return "cannot travel that far"}}
